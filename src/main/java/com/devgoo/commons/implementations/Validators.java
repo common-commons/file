d3d6baf5fef4f67ca09066e5a4a9153e0ae978c0
madimetja <mmjshika@gmail.com>
2016-09-03 00:31:26 +0200

    Refactored validator class

--- src/main/java/com/devgoo/commons/implementations/Validators.java
+++ src/main/java/com/devgoo/commons/implementations/Validators.java
@@ -10,24 +10,36 @@ import org.json.JSONObject;
  * Created by chrismipi on 2016/09/02.
  */
 public class Validators implements ValidatorInterface {
+
 	@Override
 	public boolean validate(String content, FileFormats format) {
 		if(content == null) return false;
 		switch (format) {
 			case JSON:
-				try {
-					new JSONObject(content.trim());
-				} catch (JSONException ex) {
-					try {
-						new JSONArray(content.trim());
-					} catch (JSONException ex1) {
-						return false;
-					}
-				}
-				return true;
-
+				return validateJsonFormat(content);
 			default:
 				return false;
 		}
+	}
+
+	/**
+	 * Validates that the given content is valid JSON format.
+	 *
+	 * @param content The content to be validated.
+	 *
+	 * @return Returns a boolean value representing the validity
+	 * of the file.
+	 */
+	private boolean validateJsonFormat(String content){
+		try {
+			new JSONObject(content.trim());
+		} catch (JSONException ex) {
+			try {
+				new JSONArray(content.trim());
+			} catch (JSONException ex1) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
