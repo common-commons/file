341c1e0856b2379f8f30167b0b3913d3d3cd6944
madimetja <mmjshika@gmail.com>
2016-09-02 16:12:25 +0200

    Added contract to the parser interface

--- src/main/java/com/devgoo/commons/interfaces/ParserInterface.java
+++ src/main/java/com/devgoo/commons/interfaces/ParserInterface.java
@@ -1,10 +1,22 @@
 package com.devgoo.commons.interfaces;
 
+import com.devgoo.commons.util.FileFormats;
+
+import java.io.File;
+
 /**
  * Created by madimetja on 2016/09/02.
  */
 public interface ParserInterface {
 
-
+	/**
+	 * Parses a given file on the file system.
+	 *
+	 * @param absoluteFilePath The absolute path to the file to be parsed.
+	 * @param fileFormat The format of the file to be parsed.
+	 *
+	 * @return Returns a Java IO File instance of the file parsed.
+	 */
+	File parseFile(String absoluteFilePath, FileFormats fileFormat);
 
 }
