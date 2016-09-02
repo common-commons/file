e7dc1645635d4ed1beac16db46674b4074ae5f95
madimetja <mmjshika@gmail.com>
2016-09-02 17:16:55 +0200

    Initialized phatfile wrapper class

--- src/main/java/com/devgoo/commons/wrapper/PhatFile.java
+++ src/main/java/com/devgoo/commons/wrapper/PhatFile.java
@@ -1,9 +1,29 @@
 package com.devgoo.commons.wrapper;
 
 import java.io.File;
+import java.net.URI;
 
 /**
  * Created by madimetja on 2016/09/02.
  */
 public class PhatFile extends File {
+
+
+	public PhatFile(String pathname) {
+		super(pathname);
+	}
+
+	public PhatFile(String parent, String child) {
+		super(parent, child);
+	}
+
+	public PhatFile(File parent, String child) {
+		super(parent, child);
+	}
+
+	public PhatFile(URI uri) {
+		super(uri);
+	}
+
+
 }
