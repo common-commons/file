8ceee784a47489baaa4f2159a0f25315605bf416
chrismipi <christopher.mipi@gmail.com>
2016-09-02 17:24:26 +0200

    adding tests

--- src/test/java/com/devgoo/commons/writter/GenerateFileTest.java
+++ src/test/java/com/devgoo/commons/writter/GenerateFileTest.java
@@ -5,6 +5,9 @@ import com.devgoo.commons.interfaces.WriterInterface;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 
 /**
  * Created by madimetja on 2016/09/02.
@@ -14,11 +17,12 @@ public class GenerateFileTest {
 
 	@Before
 	public void setUp() {
-		 writer = new Writer();
+		writer = new Writer();
+		assertNotNull(writer);
 	}
 
 	@Test
 	public void writeTxtFile() {
-
+		assertTrue(true);
 	}
 }
