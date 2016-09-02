f1b967123b0085170d880184d809f026932d0bee
madimetja <mmjshika@gmail.com>
2016-09-02 15:56:42 +0200

    Added the basic test class structures

---
+++ src/test/java/com/devgoo/commons/validator/ValidatorTest.java
@@ -0,0 +1,7 @@
+package com.devgoo.commons.validator;
+
+/**
+ * Created by madimetja on 2016/09/02.
+ */
+public class ValidatorTest {
+}
---
+++ src/test/java/com/devgoo/commons/writter/GenerateFileTest.java
@@ -0,0 +1,7 @@
+package com.devgoo.commons.writter;
+
+/**
+ * Created by madimetja on 2016/09/02.
+ */
+public class GenerateFileTest {
+}
