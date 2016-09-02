1cc98ad9b906ce54a39bf2a3b6d4563133005832
chrismipi <christopher.mipi@gmail.com>
2016-09-02 17:21:29 +0200

    adding the implementation of writer interface adding the file formats which ar supported

---
+++ src/main/java/com/devgoo/commons/implementations/Writer.java
@@ -0,0 +1,16 @@
+package com.devgoo.commons.implementations;
+
+import com.devgoo.commons.interfaces.WriterInterface;
+import com.devgoo.commons.util.FileFormats;
+
+import java.io.File;
+
+/**
+ * Created by chrismipi on 2016/09/02.
+ */
+public class Writer implements WriterInterface {
+	@Override
+	public File writeToFile(String fileContent, String absoluteFilePath, FileFormats outputFileFormat) {
+		return null;
+	}
+}
--- src/test/java/com/devgoo/commons/writter/GenerateFileTest.java
+++ src/test/java/com/devgoo/commons/writter/GenerateFileTest.java
@@ -1,7 +1,24 @@
 package com.devgoo.commons.writter;
 
+import com.devgoo.commons.implementations.Writer;
+import com.devgoo.commons.interfaces.WriterInterface;
+import org.junit.Before;
+import org.junit.Test;
+
+
 /**
  * Created by madimetja on 2016/09/02.
  */
 public class GenerateFileTest {
+	private WriterInterface writer;
+
+	@Before
+	public void setUp() {
+		 writer = new Writer();
+	}
+
+	@Test
+	public void writeTxtFile() {
+
+	}
 }
