3efaee6ce983706e1add22e4c34e4700eba3d364
madimetja <mmjshika@gmail.com>
2016-09-03 01:11:10 +0200

    Refactored the file format enums to have names

--- src/main/java/com/devgoo/commons/util/FileFormats.java
+++ src/main/java/com/devgoo/commons/util/FileFormats.java
@@ -4,8 +4,22 @@ package com.devgoo.commons.util;
  * Created by madimetja on 2016/09/02.
  */
 public enum FileFormats {
-	TXT,
-	CSV,
-	JSON,
-	XML
+	TXT (".txt"),
+	CSV (".csv"),
+	JSON (".json"),
+	XML (".xml");
+
+	private String fileFormat;
+
+	private FileFormats(String fileFormat){
+		this.fileFormat = fileFormat;
+	}
+
+	public String getExtension(){
+		return this.fileFormat;
+	}
+
+	public String getName(){
+		return this.name().toLowerCase();
+	}
 }
