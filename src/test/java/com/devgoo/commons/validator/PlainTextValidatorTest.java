4f068364ad2835c2945955c223836e8caa9366aa
madimetja <mmjshika@gmail.com>
2016-09-05 15:33:49 +0200

    Updated logic to handle text files

---
+++ src/test/java/com/devgoo/commons/validator/PlainTextValidatorTest.java
@@ -0,0 +1,59 @@
+package com.devgoo.commons.validator;
+
+import com.devgoo.commons.exceptions.UnknownFileFormatException;
+import com.devgoo.commons.implementations.Validators;
+import com.devgoo.commons.interfaces.ValidatorInterface;
+import com.devgoo.commons.util.FileFormats;
+import com.devgoo.commons.wrapper.PhatFile;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by madimetja on 2016/09/02.
+ */
+public class PlainTextValidatorTest {
+
+	private ClassLoader classLoader;
+
+	private ValidatorInterface validator;
+
+	private final String PATH_TO_VALID_XML_FILE = "files/validator/validXmlFile.xml";
+	private final String PATH_TO_VALID_XML_FILE_WITH_ATTRIBUTES = "files/validator/validXmlFileWithAttributes.xml";
+	private final String PATH_TO_EMPTY_XML_FILE = "files/validator/emptyXmlFile.xml";
+	private final String PATH_TO_INVALID_XML_FILE = "files/validator/invalidXmlFile.xml";
+
+	@Before
+	public void setUp() {
+		classLoader = getClass().getClassLoader();
+		validator = new Validators();
+	}
+
+	@Test
+	public void testThatValidXmlIsReadCorrectly() throws IOException, UnknownFileFormatException {
+		PhatFile testFile = new PhatFile(classLoader.getResource(PATH_TO_VALID_XML_FILE).getPath());
+		assertTrue(validator.validate(testFile.getContentAsString(), FileFormats.XML));
+	}
+
+	@Test
+	public void testThatValidXmlWithAttributesIsReadCorrectly() throws IOException, UnknownFileFormatException {
+		PhatFile testFile = new PhatFile(classLoader.getResource(PATH_TO_VALID_XML_FILE_WITH_ATTRIBUTES).getPath());
+		assertTrue(validator.validate(testFile.getContentAsString(), FileFormats.XML));
+	}
+
+	@Test
+	public void testThatAnEmptyFileIsRejected() throws IOException, UnknownFileFormatException {
+		PhatFile testFile = new PhatFile(classLoader.getResource(PATH_TO_EMPTY_XML_FILE).getPath());
+		assertFalse(validator.validate(testFile.getContentAsString(), FileFormats.XML));
+	}
+
+	@Test
+	public void testThatInvalidXmlIsRejected() throws IOException, UnknownFileFormatException {
+		PhatFile testFile = new PhatFile(classLoader.getResource(PATH_TO_INVALID_XML_FILE).getPath());
+		assertFalse(validator.validate(testFile.getContentAsString(), FileFormats.XML));
+	}
+}
